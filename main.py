e957aa9d3e2f10817155a43ebf4a473452d69567
nunopenim <nuno.penim@gmail.com>
2022-06-09 22:21:08 +0100

    main: So, you know I hate PEP8? I like Anarchy more, but oh well...

--- main.py
+++ main.py
@@ -33,14 +33,26 @@ HORZ_FLIP = True
 FILL_MODE = "constant"
 DATA_FORMAT = "channels_last"
 
-work_dr = tkr.preprocessing.image.ImageDataGenerator(rescale=1. / 255, brightness_range=BRIGHT_RANGE, zoom_range=ZOOM, data_format=DATA_FORMAT, fill_mode=FILL_MODE, horizontal_flip=HORZ_FLIP)
+work_dr = tkr.preprocessing.image.ImageDataGenerator(rescale=1. / 255,
+                                                     brightness_range=BRIGHT_RANGE,
+                                                     zoom_range=ZOOM,
+                                                     data_format=DATA_FORMAT,
+                                                     fill_mode=FILL_MODE,
+                                                     horizontal_flip=HORZ_FLIP)
+
 train_data_gen = work_dr.flow_from_directory(directory=WORK_DIR, target_size=DIM, batch_size=6500, shuffle=False)
 
 # Data balancing - Using SMOTE technique
 
 train_data, train_labels = train_data_gen.next()
-train_data, test_data, train_labels, test_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
-train_data, val_data, train_labels, val_labels = train_test_split(train_data, train_labels, test_size = 0.2, random_state=42)
+train_data, test_data, train_labels, test_labels = train_test_split(train_data,
+                                                                    train_labels,
+                                                                    test_size=0.2,
+                                                                    random_state=42)
+train_data, val_data, train_labels, val_labels = train_test_split(train_data,
+                                                                  train_labels,
+                                                                  test_size=0.2,
+                                                                  random_state=42)
 
 sm = SMOTE(random_state=42)
 
@@ -54,11 +66,11 @@ fine_dropout = tkr.layers.Dropout(0.1)
 coarse_dropout = tkr.layers.Dropout(0.5)
 flat_them_layers = tkr.layers.Flatten()
 
-layer1 = tkr.layers.Conv2D(16, (3, 3), activation = 'relu', padding = 'same', input_shape = (176,176,3))
-layer2 = tkr.layers.Conv2D(32, (3, 3), activation = 'relu', padding = 'same')
-layer3 = tkr.layers.Conv2D(64, (3, 3), activation = 'relu', padding = 'same')
-layer4 = tkr.layers.Conv2D(128, (3, 3), activation = 'relu', padding = 'same')
-out_layer = tkr.layers.Dense(units = 4, activation = 'softmax')
+layer1 = tkr.layers.Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(176, 176, 3))
+layer2 = tkr.layers.Conv2D(32, (3, 3), activation='relu', padding='same')
+layer3 = tkr.layers.Conv2D(64, (3, 3), activation='relu', padding='same')
+layer4 = tkr.layers.Conv2D(128, (3, 3), activation='relu', padding='same')
+out_layer = tkr.layers.Dense(units=4, activation='softmax')
 
 model = tkr.models.Sequential()
 model.add(layer1)
@@ -76,7 +88,7 @@ model.add(coarse_dropout)
 model.add(flat_them_layers)
 model.add(out_layer)
 
-model.compile(optimizer='adam', loss = "categorical_crossentropy" , metrics=['acc'])
+model.compile(optimizer='adam', loss="categorical_crossentropy", metrics=['acc'])
 
 # Extra model parameters/auxiliary functions
 
@@ -98,7 +110,11 @@ LearningRateScheduler = tkr.callbacks.LearningRateScheduler(scheduler)
 
 # Model Training - In here, the model is trained and validated with both the train and validation datasets
 
-history = model.fit(train_data, train_labels, validation_data=(val_data, val_labels), epochs=100, callbacks=[EarlyStopping])
+history = model.fit(train_data,
+                    train_labels,
+                    validation_data=(val_data, val_labels),
+                    epochs=100,
+                    callbacks=[EarlyStopping])
 
 # Model Metrics - Graphs on the model behaviour during it's training are presented here
 
@@ -108,13 +124,13 @@ loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(acc) + 1)
 
-plt.plot(epochs, acc, 'bo', label = 'Training acc')
-plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
 plt.title('Training and Validation accuracy')
 plt.legend()
 plt.figure()
 plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label= 'validation loss')
+plt.plot(epochs, val_loss, 'b', label='validation loss')
 plt.title('Training and Validation loss')
 plt.legend()
 
@@ -122,4 +138,4 @@ plt.show()
 
 # Model evaluation - Here the model is evaluated with the test dataset
 
-model.evaluate(test_data,test_labels)
+model.evaluate(test_data, test_labels)
